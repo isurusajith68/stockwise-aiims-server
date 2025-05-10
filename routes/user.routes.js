ad253379ffa0ce8c2aad53d0ea08f72bb7b4fca3
Isuru Sajith <89340506+isurusajith68@users.noreply.github.com>
2025-05-11 02:29:16 +0530

    refactor: Simplify user routes by removing commented code and ensuring active user verification for profile access

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,26 +1,14 @@
-const { authJwt, verifyToken, optionalAuth } = require("../middleware/authJwt");
+const { verifyActiveUser } = require("../middleware/authJwt");
 const controller = require("../controllers/user.controller");
 
 module.exports = function (app) {
-  // app.use(function (req, res, next) {
-  //   res.header(
-  //     "Access-Control-Allow-Headers",
-  //     "Origin, Content-Type, Accept, Authorization"
-  //   );
-  //   next();
-  // });
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, Content-Type, Accept, Authorization"
+    );
+    next();
+  });
 
-  // // app.get("/api/users/public", controller.publicAccess);
-
-  // app.get(
-  //   "/api/users/profile",
-  //   [authJwt.verifyActiveUser],
-  //   controller.userProfile
-  // );
-
-  // app.get("/api/users/admin", [verifyToken], controller.adminAccess);
-
-  // app.get("/api/users/content", [optionalAuth], controller.getContent);
-
-  // app.post("/api/auth/logout", [verifyToken], controller.logout);
+  app.get("/api/users/profile", [verifyActiveUser], controller.getProfile);
 };
